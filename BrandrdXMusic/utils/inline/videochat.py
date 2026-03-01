f3ea152b7ef7ef42e658a75cebb9c298e7366848
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-01 20:01:21 +0530

    Update videochat.py

--- BrandrdXMusic/utils/inline/videochat.py
+++ BrandrdXMusic/utils/inline/videochat.py
@@ -1,13 +1,20 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def get_buttons():
-  return InlineKeyboardMarkup(
+def videochat_panel(_):
+    return InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("• ᴠɪᴅєσᴄʜᴧᴛ •", callback_data="help_category videochat"),
-                InlineKeyboardButton(" • ꜰᴜη •", callback_data="help_category fun"),
+                InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6"),
+                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),
+                InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12"),
             ],
             [
-                InlineKeyboardButton("• ϻᴧηᴧɢєϻєηᴛ •", callback_data="help_category moderation"),
-                InlineKeyboardButton("• ꜱᴜᴅσєʀꜱ σηʟʏ •", callback_data="help_category sudoers"),
+                InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"),
+                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"),
+                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15"),
             ],
+            [
+                InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="back_to_main"),
+            ],
+        ]
+    )
