5ce8aa0cd1fcee24e91f6a874184e13aa499fde1
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-01 11:51:11 +0530

    Update bot_left.py

--- BrandrdXMusic/plugins/tools/bot_left.py
+++ BrandrdXMusic/plugins/tools/bot_left.py
@@ -14,12 +14,12 @@ from BrandrdXMusic import app
 from BrandrdXMusic.utils.database import get_assistant
 from BrandrdXMusic.utils.database import delete_served_chat
 
-photo = [
-    "https://graph.org/file/872dc8af2a36bed43b9b6.jpg",
-    "https://graph.org/file/f4b34351a59061ba1c61b.jpg",
-    "https://graph.org/file/3fb3f4c8a1250c6a50af1.jpg",
-    "https://graph.org/file/eabab7e8a3e5df87a0b04.jpg",
-    "https://graph.org/file/427f4869a158126957747.jpg",
+video = [
+    "https://files.catbox.moe/h1xsni.mp4",
+    "https://files.catbox.moe/h1xsni.mp4",
+    "https://files.catbox.moe/h1xsni.mp4",
+    "https://files.catbox.moe/h1xsni.mp4",
+    "https://files.catbox.moe/h1xsni.mp4",
 ]
 
 
@@ -39,7 +39,7 @@ async def on_left_chat_member(_, message: Message):
             )
             chat_id = message.chat.id
             left = f"✫ <b><u>#𝗟𝗘𝗙𝗧_𝗚𝗥𝗢𝗨𝗣</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝗖𝗛𝗔𝗧 𝗜𝗗 : {chat_id}\n\n𝗥𝗘𝗠𝗢𝗩𝗘𝗗 𝗕𝗬 : {remove_by}\n\n𝗕𝗢𝗧 : @{app.username}"
-            await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
+            await app.send_video(LOG_GROUP_ID, video=random.choice(video), caption=left)
             await delete_served_chat(chat_id)
             await userbot.leave_chat(chat_id)
     except Exception as e:
