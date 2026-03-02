b9881f18dcf81958f26e2b56b0c892d757c621a8
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-02 14:57:42 +0530

    Update fun.py

--- BrandrdXMusic/utils/inline/fun.py
+++ BrandrdXMusic/utils/inline/fun.py
@@ -1,4 +1,5 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from BrandrdXMusic import app
 
 def fun_panel(_):
     return InlineKeyboardMarkup(
