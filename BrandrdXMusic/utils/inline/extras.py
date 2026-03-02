17f61bbf240cae734cf599ab6879f4615a1ad5ec
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-02 14:56:39 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/yukieee_03"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
