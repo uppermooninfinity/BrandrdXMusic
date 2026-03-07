4a463cafcfa7f8d678f21fff642c54ef36c3a02d
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-08 01:49:10 +0530

    Update pastebin.py

--- BrandrdXMusic/utils/pastebin.py
+++ BrandrdXMusic/utils/pastebin.py
@@ -40,7 +40,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def INFIBIN(text):
+async def HottyBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
