ef6fe8148da48bf276250178c553b5c87ceb7326
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-08 01:44:27 +0530

    Update pastebin.py

--- BrandrdXMusic/utils/pastebin.py
+++ BrandrdXMusic/utils/pastebin.py
@@ -1,4 +1,8 @@
-import socket,requests
+
+import aiohttp
+
+
+import socket
 from asyncio import get_running_loop
 from functools import partial
 
@@ -16,9 +20,29 @@ def _netcat(host, port, content):
     s.close()
 
 
-async def HottyBin(content):
-    url ="https://pastebin.com/api/api_post.php"
-    data = {"api_dev_key":"9Rfu50iV5l3EuRWATw7EDLuC37RED-C4","api_paste_code": content,"api_option": "paste"}
-    response = requests.post(url, data=data)
-    link=response.text
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
+    return link
+
+####2nd paste code 
+
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def INFIBIN(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
     return link
