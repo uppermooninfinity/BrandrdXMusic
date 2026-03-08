9a5001b1f910e495b2220609ee1b7f46291492df
uppermooninfinity <uppermooninfinity@gmail.com>
2026-03-08 12:08:42 +0530

    Update watcher.py

--- BrandrdXMusic/plugins/misc/watcher.py
+++ BrandrdXMusic/plugins/misc/watcher.py
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Hotty.stop_stream_force(message.chat.id)
+    await Hotty.force_stop_stream(message.chat.id)
